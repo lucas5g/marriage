86fc14f9173a1f5feef731c62a9f78301688747a
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-04-29 13:23:05 -0300

    add seed sale

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,20 +2,27 @@ import { prisma } from "@/libs/prisma";
 
 
 
-function main(){
-  prisma.product.upsert({
-    where: {
-      id: 1
-    },
-    create:{
-      image: 'https://72380.cdn.simplo7.net/static/72380/sku/eletricas-furadeiras-manuais-e-base-magnetica-furadeira-parafusadeira-impacto-1-2-20v-dcd7781-b-dewalt-1684956458288.jpg',
-      name: 'furadeira',
-      price: 599.9,
-      id: 1
-    },
-    update:{}
-  }).then(() => console.log('Product created successfully'))
-}
+prisma.product.upsert({
+  where: {
+    id: 1
+  },
+  create: {
+    image: 'https://72380.cdn.simplo7.net/static/72380/sku/eletricas-furadeiras-manuais-e-base-magnetica-furadeira-parafusadeira-impacto-1-2-20v-dcd7781-b-dewalt-1684956458288.jpg',
+    name: 'furadeira',
+    price: 599.9,
+    id: 1
+  },
+  update: {}
+}).then(() => console.log('Product created successfully'))
 
+prisma.sale.upsert({
+  where:{
+    id: 1
+  },
+  create:{
+    emailOrName:'lucas@mail.com',
+    totalPrice: 19,
+  },
+  update:{}
+}).then(() => console.log('Sale created successfully'))
 
-main()
